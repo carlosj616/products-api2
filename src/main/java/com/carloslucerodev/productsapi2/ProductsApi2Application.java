413004d3c1ae738e5adc523e28ac36b8938bad09
Carlos J <carlosj616@outlook.com>
2023-09-18 11:41:59 -0500

    first commit

---
+++ src/main/java/com/carloslucerodev/productsapi2/ProductsApi2Application.java
@@ -0,0 +1,13 @@
+package com.carloslucerodev.productsapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/carloslucerodev/productsapi2/ProductsApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloslucerodev.productsapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
